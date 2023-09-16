35cde6f3be6c242f39e87c63e6a84ae9df14b655
Tom Harding <tomjharding@live.co.uk>
2023-09-17 00:31:42 +0200

    The Rust Programming Langugae, Chapter 12

---
+++ the-rust-programming-language/12-an-io-project-building-a-command-line-program/src/lib.rs
@@ -0,0 +1,83 @@
+use std::env;
+use std::error::Error;
+use std::fs;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
+}
+
+impl Config {
+    pub fn build(arguments: &[String]) -> Result<Config, &'static str> {
+        let query =
+                arguments.get(0)
+                .ok_or("Not enough arguments!")?
+                .to_string();
+
+        let file_path =
+                arguments.get(1)
+                .ok_or("Not enough arguments!")?
+                .to_string();
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        Ok(Config { query, file_path, ignore_case })
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents.lines().filter(|line| line.contains(query)).collect()
+}
+
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+
+    contents.lines().filter(|line| line.to_lowercase().contains(&query)).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
+    }
+}
---
+++ the-rust-programming-language/12-an-io-project-building-a-command-line-program/src/main.rs
@@ -0,0 +1,17 @@
+use the_rust_programming_language_12_an_io_project_building_a_command_line_program as minigrep;
+
+use std::env;
+use std::process;
+
+fn main () {
+    let arguments: Vec<String> = env::args().collect();
+    let config = minigrep::Config::build(&arguments).unwrap_or_else(|error| {
+        eprintln!("Oh no: {error}");
+        process::exit(1);
+    });
+
+    if let Err(error) = minigrep::run(config) {
+        eprintln!("Oh no again: {error}");
+        process::exit(1);
+    }
+}
